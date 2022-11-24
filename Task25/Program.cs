67c23bdb7fc6552d8ef8ea1a5b911290424e0903
Maria <veiershtrass@inbox.ru>
2022-11-25 00:08:05 +0300

    Решение задачи № 25

---
+++ Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в
+// натуральную степень B.
+// Не используя Math.Pow
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число, которое необходимо возвести в натуральную степень");
+String str1 = Console.ReadLine();
+Console.WriteLine("Введите натуральное число");
+String str2 = Console.ReadLine();
+int A = int.Parse(str1);
+int B = int.Parse(str2);
+int x = 1;
+for (int i = 1; i <= B; i++)
+{
+    x = x * A;
+}
+Console.WriteLine(x);
